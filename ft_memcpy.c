31b11a7e7393bbd0572cf337d1c668fc9483a0ec
AOUKY <aoukyhamza@gmail.com>
2023-12-14 12:55:29 +0100

    done

---
+++ ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haouky <haouky@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 17:22:58 by haouky            #+#    #+#             */
+/*   Updated: 2023/12/12 17:53:26 by haouky           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*dp;
+	const unsigned char	*sp;
+
+	if (!dst && !src)
+		return (0);
+	dp = dst;
+	sp = src;
+	i = 0;
+	while (i < n)
+	{
+		dp[i] = sp[i];
+		i++;
+	}
+	return (dst);
+}
