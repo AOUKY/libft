5529c97b24cec9f2f7f6ec521a2ee9a584308676
AOUKY <aoukyhamza@gmail.com>
2023-12-29 23:05:17 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -22,7 +22,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (a[i] == (unsigned char)c)
-			return ((void *)&s[i]);
+			return ((void *)&a[i]);
 		i++;
 	}
 	return (0);
